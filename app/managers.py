eac3bf5d28029e853b716a3744d9423c227f7980
Liubov Kobylianska <blondblanc3@gmail.com>
2026-04-30 22:21:52 -0500

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,37 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self.cursor.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name)"
+            f" VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self.conn.commit()
+
+    def all(self) -> list[Actor]:
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        rows = self.cursor.fetchall()
+        return [Actor(row[0], row[1], row[2]) for row in rows]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self.cursor.execute(
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ?"
+            f" WHERE id = ?",
+            (new_first_name, new_last_name, pk)
+        )
+        self.conn.commit()
+
+    def delete(self, pk: int) -> None:
+        self.cursor.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,)
+        )
+        self.conn.commit()
